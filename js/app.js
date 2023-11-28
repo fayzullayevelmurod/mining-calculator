5605fd42f2270374efd4327befc6fc88c77686b6
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-11-28 20:04:03 +0500

    the end

--- js/app.js
+++ js/app.js
@@ -3,3 +3,22 @@ var swiper = new Swiper(".usefulSlider", {
     centeredSlides: true,
     spaceBetween: 24,
 });
+var swiper = new Swiper(".usefulSliderTwo", {
+    slidesPerView: 2,
+    // centeredSlides: true,
+    spaceBetween: 24,
+    breakpoints: {
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 15
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 15
+        },
+        993: {
+          slidesPerView: 4,
+          spaceBetween: 20
+        }
+      }
+});
